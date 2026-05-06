28975691263cbcb281181620a3457f026dc17660
Isabela Pak Bragagnolo <162496392+cc24316@users.noreply.github.com>
2026-05-06 15:09:44 -0300

    Update InversorListas.py

--- program/InversorListas.py
+++ program/InversorListas.py
@@ -1,1 +1,10 @@
+print("programa para inverter lista")
 
+def inversao (L):
+    ret=[]
+    for elemento in L:
+        ret = [elemento]+ret
+    return ret
+    
+    
+print(inversao([5,4,3,2,56]))
